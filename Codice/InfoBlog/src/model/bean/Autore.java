91f26514e5d69dc59b7e864f3934efdbd4c7eed0
lodo744 <55733870+lodo744@users.noreply.github.com>
2020-01-08 10:19:09 +0100

    Implementazione del bean autore

---
+++ Codice/InfoBlog/src/model/bean/Autore.java
@@ -0,0 +1,81 @@
+package model.bean;
+
+public class Autore 
+{
+	private String email;
+	private String password;
+	private String nome;
+	private String cognome;
+	private String username;
+	
+	public Autore()
+	{
+		
+	}
+	
+	public Autore(String email, String password, String nome, String cognome, String username) 
+	{
+		this.email = email;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+	}
+
+	public String getEmail() 
+	{
+		return email;
+	}
+
+	public String getPassword() 
+	{
+		return password;
+	}
+
+	public String getNome() 
+	{
+		return nome;
+	}
+
+	public String getCognome() 
+	{
+		return cognome;
+	}
+
+	public String getUsername() 
+	{
+		return username;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+
+	public void setNome(String nome) 
+	{
+		this.nome = nome;
+	}
+
+	public void setCognome(String cognome) 
+	{
+		this.cognome = cognome;
+	}
+
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+	
+	public String toString()
+	{
+		return this.getClass().getSimpleName() + "[email = " + email + ", password = " + password +
+				", nome = " + nome + ", cognome = " + cognome + ", username = " + username + "]";
+		
+	}
+	
+}
