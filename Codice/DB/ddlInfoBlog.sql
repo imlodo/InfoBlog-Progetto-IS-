9d7dbff474bda48860a0d552231e8d52a689a3c1
lodo744 <55733870+lodo744@users.noreply.github.com>
2020-01-08 20:31:06 +0100

    change folder

--- Codice/DB/ddlInfoBlog.sql
+++ Codice/DB/ddlInfoBlog.sql
@@ -7,39 +7,52 @@ use InfoBlog;
 
 Create table utente(
 email varchar(50),
-password varchar(32) not null,
+password varchar(30) not null,
 nome varchar(30) not null,
 cognome	varchar(30) not null,
-username varchar(20) not null,
+username varchar(30) not null,
 primary key(email)
 );
 
 Create table autore(
 email varchar(50),
-password varchar(32) not null,
+password varchar(30) not null,
 nome varchar(30) not null,
 cognome	varchar(30) not null,
-username varchar(20) not null,
+username varchar(30) not null,
 primary key(email)
 );
 
 
 Create table moderatore(
 email varchar(50),
-password varchar(32) not null,
+password varchar(30) not null,
 nome varchar(30) not null,
 cognome	varchar(30) not null,
-username varchar(20) not null,
+username varchar(30) not null,
 categoriaModerazione varchar(30) not null,
 primary key(email)
 );
 
+create table seguace(
+utente varchar(50),
+foreign key (utente) references utente(email)
+on update cascade
+on delete cascade,
+autore varchar(50),
+foreign key (autore) references autore(email)
+on update cascade
+on delete cascade,
+primary key (utente,autore)
+);
+
 create table articolo(
-Titolo varchar(40) not null,
+Titolo varchar(50) not null,
 id int auto_increment,
 categoria varchar(20) not null,
-contenuto varchar(500) not null,
+contenuto varchar(15000) not null,
 stato enum('daPubblicare','inModerazione','sottoModerazione','Pubblicato') not null default 'daPubblicare',
+dataPubblicazione date,
 scrittore varchar(50),
 foreign key (scrittore) references autore(email)
 on update cascade
@@ -52,7 +65,7 @@ primary key(id)
 );
 
 create table notifica(
-contenuto varchar(30) not null,
+contenuto varchar(700) not null,
 id int auto_increment,
 stato enum('inviato','letto') default 'inviato',
 autore varchar(50),
@@ -67,19 +80,37 @@ primary key(id,autore,moderatore)
 );
 
 create table messaggio(
-contenuto varchar (500) not null,
+contenuto varchar (700) not null,
 id int auto_increment,
 stato enum('inviato','letto') default 'inviato',
-autore varchar(50),
-foreign key (autore) references autore(email)
+data datetime,
+mittente varchar(50),
+foreign key (mittente) references utente(email)
 on update cascade
 on delete cascade,
-utente varchar(50),
-foreign key (utente) references utente(email)
+destinatario varchar(50),
+foreign key (destinatario) references autore(email)
 on update cascade
 on delete cascade,
 primary key (id)
 );
+
+create table risposta(
+contenuto varchar (700) not null,
+id int auto_increment,
+stato enum('inviato','letto') default 'inviato',
+data datetime,
+mittente varchar(50),
+foreign key (mittente) references autore(email)
+on update cascade
+on delete cascade,
+destinatario varchar(50),
+foreign key (destinatario) references utente(email)
+on update cascade
+on delete cascade,
+primary key (id)
+);
+
 
 create table commento(
 valutazione varchar(300) not null,
@@ -109,10 +140,10 @@ primary key (id,email)
 
 create table evento(
 data date not null,
-via varchar(40) not null,
-città varchar(40) not null,
+via varchar(30) not null,
+città varchar(30) not null,
 nome varchar(50) not null,
-argomento varchar(50) not null,
+argomento varchar(500) not null,
 id int auto_increment,
 email varchar(50),
 foreign key (email) references autore(email)
@@ -122,7 +153,7 @@ primary key(id,data,via,città)
 );
 
 create table allegato(
-percorsoFile varchar(50) not null,
+percorsoFile varchar(100) not null,
 primary key(percorsoFile),
 id int,
 foreign key (id) references articolo(id)
