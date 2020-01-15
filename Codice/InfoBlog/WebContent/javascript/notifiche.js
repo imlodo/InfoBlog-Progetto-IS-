9181df082c41fdda35ba6c6e09ae193f79315a13
FedericoSiano <f.siano18@studenti.unisa.it>
2020-01-15 14:40:50 +0100

    Modifica ad header.jsp e notifiche.js

--- Codice/InfoBlog/WebContent/javascript/notifiche.js
+++ Codice/InfoBlog/WebContent/javascript/notifiche.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 			}
 		}
 	});
+	//per autore
 	$("#itemNotifiche").click(function () {
 			var menu=document.getElementById("lista");
 			if(menu){
@@ -23,19 +24,47 @@ $(document).ready(function(){
 						$("#lista").append("<div style='display:flex; color:black; border-style:solid; border-width:thin;'>Non ci sono notifiche...</div>");
 					}
 					for(var i=0;i<jsons.length;i++){
-						$("#lista").append("<li id='notifica"+i+"' class='item'/>")
-						if(jsons[i].stato==="letto"){
-							$("#notifica"+i+"").append("<div class='stato'><span>(LETTO)</span></div>");
+						if(jsons[i].contenuto.indexOf("Richiesta_pubblicazione_articolo") >= 0){
+							$("#lista").append("<li id='notifica"+i+"' class='item'/>")
+							if(jsons[i].stato==="letto"){
+								$("#notifica"+i+"").append("<div class='stato'><span>(LETTO)</span></div>");
+							}
+							else{
+								$("#notifica"+i+"").append("<div class='stato'><span>(NUOVO)</span></div>");
+							}
+							$("#notifica"+i+"")
+								.append("<div class='testo'><a href='/InfoBlog/LetturaNotificaControl?idNotifica="+jsons[i].id+"'>"+jsons[i].contenuto+"</a></div>");
 						}
-						else{
-							$("#notifica"+i+"").append("<div class='stato'><span>(NUOVO)</span></div>");
-						}
-						$("#notifica"+i+"")
-							.append("<div class='testo'><a href='/InfoBlog/LetturaNotificaControl?idNotifica="+jsons[i].id+"'>"+jsons[i].contenuto+"</a></div>");
 					}
 				});
 			}
 	});
+	//per moderatore
+	$("#itemNotificheMod").click(function () {
+		var menu=document.getElementById("lista");
+		if(menu){
+			menu.remove();
+		}
+		else{
+			$("#itemNotificheMod").append("<ul id='lista'/>");
+			$.post("/InfoBlog/PrelevaNotificheControl",{e_mail:email},function(jsons){
+				if(jsons.length==0){
+					$("#lista").append("<div style='display:flex; color:black; border-style:solid; border-width:thin;'>Non ci sono notifiche...</div>");
+				}
+				for(var i=0;i<jsons.length;i++){
+					$("#lista").append("<li id='notifica"+i+"' class='item'/>")
+					if(jsons[i].stato==="letto"){
+						$("#notifica"+i+"").append("<div class='stato'><span>(LETTO)</span></div>");
+					}
+					else{
+						$("#notifica"+i+"").append("<div class='stato'><span>(NUOVO)</span></div>");
+					}
+					$("#notifica"+i+"")
+						.append("<div class='testo'><a href='/InfoBlog/LetturaNotificaControl?idNotifica="+jsons[i].id+"'>"+jsons[i].contenuto+"</a></div>");
+				}
+			});
+		}
+});
 	$(".testo").click(function (){
 		var number = parseInt($(".numeroNotifiche").text(), 10);
 		if(number>0)
